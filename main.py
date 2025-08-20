b41af9e49d69344bf0c19578644a56e4539e87c3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-20 18:16:23 -0300

    feat: implement todo list management menu

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-print('----TODO List----')
-print('--------')
+from addTask import addTask
+from deleteTask import deleteTask
+from listTasks import listTasks
 
-task = input('Informe o nome da tarefa: ')
+
+while True:
+    print('\n')
+    print('Minha lista de tarefas')
+    print('----------------------')
+    print('1 - Adicionar uma nova tarefa')
+    print('2 - Remover uma tarefa')
+    print('3 - Listar minhas tarefas')
+    print('4 - Sair')
+
+    choice = input('Escolha uma opção: ')
+
+    if (choice == '1'):
+        addTask()
+    elif (choice == '2'):
+        deleteTask()
+    elif (choice == '3'):
+        listTasks()
+    elif (choice == '4'):
+        break
+    else:
+        print('Escolha inválida!')
